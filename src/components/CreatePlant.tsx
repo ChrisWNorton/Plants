6bc00edc572074327b067085fb15311ec8e7636a
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-05 17:55:34 -0700

    build out plant adding form

--- src/components/CreatePlant.tsx
+++ src/components/CreatePlant.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Modal, Form } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css'
 
 interface CreatePlantProps{
     modalOpen: boolean
@@ -7,6 +10,10 @@ interface CreatePlantProps{
 }
 
 function CreatePlant({modalOpen, handleHide}: CreatePlantProps) {
+
+    const [lastWateringDate, setLastWateringDate] = useState<Date | Date[] | null>(new Date())
+    const [nextWateringDate, setNextWateringDate] = useState<Date | Date[] | null>(new Date())
+
     return (
         <Modal show={modalOpen} onHide = {() => handleHide(false)}>
         <Modal.Header closeButton>
@@ -16,10 +23,15 @@ function CreatePlant({modalOpen, handleHide}: CreatePlantProps) {
             <Form>
                 <Form.Label>Name </Form.Label>
                 <Form.Control></Form.Control>
-                <Form.Label>When was the last time it was watered </Form.Label>
+                <Form.Label>When was the last time it was watered?</Form.Label>
+                <br/>
+                <DatePicker value= {lastWateringDate?.toString()} onChange={date => setLastWateringDate(date)} showTimeSelect/>
+                <br/>
+                <Form.Label>When is the next time it should be watered?</Form.Label>
+                <br/>
+                <DatePicker value= {nextWateringDate?.toString()} onChange={date => setNextWateringDate(date)} showTimeSelect/>
+                <br />
                 
-
-
             </Form>
 
         </Modal.Body>
