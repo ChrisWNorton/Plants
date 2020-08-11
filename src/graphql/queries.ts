9342c0df2bd06d68056dcb3648e7a4fe5036527a
ChrisWNorton <chriswsnorton@gmail.com>
2020-08-10 18:43:39 -0700

    add generated queries

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPlant = /* GraphQL */ `
+  query GetPlant($id: ID!) {
+    getPlant(id: $id) {
+      id
+      name
+      lastWatered
+      nextWatered
+      photo
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPlants = /* GraphQL */ `
+  query ListPlants(
+    $filter: ModelPlantFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPlants(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        lastWatered
+        nextWatered
+        photo
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePlant = /* GraphQL */ `
+  subscription OnCreatePlant {
+    onCreatePlant {
+      id
+      name
+      lastWatered
+      nextWatered
+      photo
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePlant = /* GraphQL */ `
+  subscription OnUpdatePlant {
+    onUpdatePlant {
+      id
+      name
+      lastWatered
+      nextWatered
+      photo
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePlant = /* GraphQL */ `
+  subscription OnDeletePlant {
+    onDeletePlant {
+      id
+      name
+      lastWatered
+      nextWatered
+      photo
+      createdAt
+      updatedAt
+    }
+  }
+`;
